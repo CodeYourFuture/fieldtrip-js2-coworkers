3846fceca1b1f22d1b64be028b706cc234f12789
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-01-13 23:04:25 +0000

    Set up react app

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,18 @@
 import { ApplicationFunctionOptions, Probot, run } from "probot";
-import { webapp } from "./webapp";
+import { api } from "./api";
 import { probot } from "./probot";
+import { webapp } from "./webapp";
 
 export const server = (
   app: Probot,
   { getRouter }: ApplicationFunctionOptions
 ) => {
-  const router = getRouter!("/app");
+  const apiRouter = getRouter!("/api");
+  const appRouter = getRouter!("/");
 
   probot(app);
-  webapp(router, app);
+  api(apiRouter, app);
+  webapp(appRouter);
 };
 
 run(server);
