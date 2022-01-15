9da51b1cd61f0623e6b6c5b1a3192aa7ae0857e4
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-01-15 11:47:51 +0000

    Set up tailwind production build

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import { Redirect, Router } from "@reach/router";
 import { Home } from "src/pages";
 import "src/assets/defaults.css";
-import "src/assets/tailwind.output.css";
 
 ReactDOM.render(
   <React.StrictMode>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,32 +1,7 @@
-module.exports = {
+global.tailwind = {};
+require("./public/tailwind.cdn.config");
+
+module.exports = Object.assign(tailwind.config, {
   content: ["./build/**/*.{html,js}"],
-  important: true,
-  theme: {
-    fontFamily: {
-      heading: "Rubik",
-    },
-    typography: {
-      default: {
-        css: {
-          html: {
-            color: "#111",
-          },
-          h1: {
-            fontFamily: "Rubik",
-          },
-          h2: {
-            fontFamily: "Rubik",
-          },
-          h3: {
-            fontFamily: "Rubik",
-          },
-        },
-      },
-    },
-    extend: {},
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/custom-forms"),
-  ],
-};
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+});
