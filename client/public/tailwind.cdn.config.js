b91eba36653371e6e6544cbf98454e9307890ae5
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-01-15 11:47:51 +0000

    Set up tailwind production build

---
+++ client/public/tailwind.cdn.config.js
@@ -0,0 +1,24 @@
+tailwind.config = {
+  important: true,
+  theme: {
+    fontFamily: {
+      heading: "Rubik",
+    },
+    typography: {
+      default: {
+        css: {
+          h1: {
+            fontFamily: "Rubik",
+          },
+          h2: {
+            fontFamily: "Rubik",
+          },
+          h3: {
+            fontFamily: "Rubik",
+          },
+        },
+      },
+    },
+    extend: {},
+  },
+};
