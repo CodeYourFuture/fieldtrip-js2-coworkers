faf81c5853d26a180b6b92ca247544a90388acf8
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-01-15 15:19:38 +0000

    RTM and simplify tailwind setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,32 @@
-global.tailwind = {};
-require("./public/tailwind.cdn.config");
-
-module.exports = Object.assign(tailwind.config, {
-  content: ["./build/**/*.{html,js}"],
+const path = require("path");
+module.exports = {
+  content: [
+    path.join(__dirname, "./src/**/*.{js,jsx,ts,tsx,md,mdx}"),
+    path.join(
+      __dirname,
+      "../node_modules/@djgrant/components/src/**/*.{ts,tsx}"
+    ),
+  ],
+  important: true,
+  theme: {
+    fontFamily: {
+      heading: "Rubik",
+    },
+  },
+  typography: {
+    default: {
+      css: {
+        h1: {
+          fontFamily: "Rubik",
+        },
+        h2: {
+          fontFamily: "Rubik",
+        },
+        h3: {
+          fontFamily: "Rubik",
+        },
+      },
+    },
+  },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
-});
+};
