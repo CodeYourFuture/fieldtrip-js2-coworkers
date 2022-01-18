ff6ab8baa41540ae4ab597f8a1948a56c1d02682
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-01-18 11:50:30 +0000

    feat: sign in to enroll button

--- apps/client/src/pages/course.tsx
+++ apps/client/src/pages/course.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react";
-import type { RouteComponentProps } from "@reach/router";
+import { RouteComponentProps } from "@reach/router";
 import { observer } from "mobx-react-lite";
 import { AppLayout } from "src/components/templates";
 import {
@@ -13,11 +13,13 @@ import {
   TabPage,
 } from "src/components/library";
 import { ICourse } from "src/models";
+import { useMst } from "src/store";
 
 type Props = { course: ICourse };
 
 export const Course: FC<RouteComponentProps & Props> = observer(
   ({ course }) => {
+    const { user } = useMst();
     return (
       <AppLayout>
         <section className="mb-8 bg-slate-50">
@@ -26,14 +28,19 @@ export const Course: FC<RouteComponentProps & Props> = observer(
               <H4 className="mb-1.5">{course.module} Module Project</H4>
               <H1>{course.title}</H1>
             </div>
-            {course.status &&
-              (course.status?.active ? (
+            {user ? (
+              course.status?.active ? (
                 <Button variant="outline" disabled>
                   Course started
                 </Button>
               ) : (
                 <Button onClick={course.enroll}>Start Project</Button>
-              ))}
+              )
+            ) : (
+              <Button onClick={() => (window.location.href = "/auth/login")}>
+                Sign in to enroll
+              </Button>
+            )}
             <div className="w-2/3 text-slate-500">
               <Markdown>{course.summary}</Markdown>
             </div>
