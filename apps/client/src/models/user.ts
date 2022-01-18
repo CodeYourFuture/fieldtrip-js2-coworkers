cf7dd6a24b5a4c0de7af95134d06c3c0538f40a7
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-01-18 11:50:51 +0000

    chore: faster types for models

--- apps/client/src/models/user.ts
+++ apps/client/src/models/user.ts
@@ -1,6 +1,11 @@
+import type { Instance, SnapshotIn, SnapshotOut } from "mobx-state-tree";
 import { types } from "mobx-state-tree";
 
 export const User = types.model({
   login: types.string,
   avatar_url: types.string,
 });
+
+export interface IUser extends Instance<typeof User> {}
+export interface IUserSnapshotIn extends SnapshotIn<typeof User> {}
+export interface IUserSnapshotOut extends SnapshotOut<typeof User> {}
