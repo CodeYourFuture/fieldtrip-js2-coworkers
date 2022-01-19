e08a59b119d7f09f15450a74431e1c979b4039cc
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-01-19 17:20:00 +0000

    chore: let server declare course types

---
+++ apps/server/src/types.ts
@@ -0,0 +1,19 @@
+export type CourseConfig = {
+  id: string;
+  title: string;
+  module: string;
+  summary: string;
+  stages: Stage[];
+};
+
+export type Stage = {
+  key: string;
+  label: string;
+  summary: string;
+  actions: Action[];
+};
+
+export type Action = {
+  label: string;
+  url: string;
+};
--- courses/js2-coworkers/config.js
+++ courses/js2-coworkers/config.js
@@ -1,12 +1,23 @@
 // @ts-check
-/** @type {import('../index').CourseConfig} */
 
+/** @type {import('../index').Stage} */
+const week1 = {
+  key: "week-1",
+  label: "Week 1",
+  summary: "./week1.md",
+  actions: [
+    { label: "Add MalachiBot to your repo", url: "/auth/install/malachi" },
+    { label: "Add AmberBot to your repo", url: "/auth/install/amber" },
+  ],
+};
+
+/** @type {import('../index').CourseConfig} */
 const config = {
   id: "js2",
   title: "Co-worker Discovery Tools",
   module: "JS2",
   summary: "./summary.md",
-  stages: [{ key: "week-1", label: "Week 1", summary: "./week1.md" }],
+  stages: [week1],
 };
 
 module.exports = config;
