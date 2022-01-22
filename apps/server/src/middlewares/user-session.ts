bd1fcaa0f39b12ce9e711cd61eeb8cc8d065aa2f
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-01-22 13:42:18 +0000

    fix: types

--- apps/server/src/middlewares/user-session.ts
+++ apps/server/src/middlewares/user-session.ts
@@ -2,7 +2,7 @@ import type { RequestHandler } from "express";
 import { ProbotOctokit } from "probot";
 
 export const userSession: RequestHandler = async (req, _, next) => {
-  req.locals = { user: null, bots: {} };
+  req.locals = { user: null, bots: {}, enrollment: null };
 
   if (req.session.user?.auth) {
     const { auth } = req.session.user;
--- apps/server/src/types/locals.ts
+++ apps/server/src/types/locals.ts
@@ -18,7 +18,7 @@ export type Locals = {
       };
   enrollment: {
     repoUrl: string;
-  };
+  } | null;
 };
 
 export type User = {
