8b0820ffe2c7656d214db7657b11b52cbf780d59
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-01-24 22:15:52 +0000

    chore: handle triggers in root bot

--- apps/server/src/bots/malachi.ts
+++ apps/server/src/bots/malachi.ts
@@ -1,47 +1,18 @@
 import { Probot } from "probot";
-import { emitter } from "../emitter";
-import { Event, Course, Metadata, createProbot } from "../utils";
-import { course, actions, repo } from "../course";
+import { Event, createProbot } from "../utils";
+import { actions, repo } from "../course";
 import { bots } from "../config";
 
 export const app = (app: Probot) => {
   app.on(
     ["installation_repositories.added", "installation.created"],
     async (context) => {
+      await new Promise((resolve) => setTimeout(resolve, 500));
       const event = new Event(context, repo);
       if (event.shouldBeIgnored) return;
-      // @todo await repo creation (I think there might be a race condition here)
       await actions.malachi.setup(event);
     }
   );
-
-  const triggerActions = Course.getTriggers(course);
-  for (const action of triggerActions) {
-    const { event, handler } = action.passed;
-    app.on(event as any, async (context) => {
-      if (context.payload.repository.name !== repo) return;
-
-      const passed = (handler as any)(context.payload);
-      if (!passed) return;
-
-      const metadataIssue = {
-        owner: context.payload.repository.owner.login,
-        repo,
-        issue_number: 1,
-      };
-      const metadata = new Metadata(context.octokit, metadataIssue);
-      const added = await metadata.add("triggers", action.id);
-
-      if (added) {
-        emitter.emit("clientUpdate", {
-          type: "trigger:passed",
-          username: context.payload.sender.login,
-          repo,
-          actionId: action.id,
-        });
-      }
-    });
-  }
 };
 
 export const instance = createProbot(bots.malachi);
--- apps/server/src/bots/root.ts
+++ apps/server/src/bots/root.ts
@@ -1,19 +1,41 @@
 import { Probot } from "probot";
-import { createProbot } from "../utils";
+import { emitter } from "../emitter";
+import { Course, Metadata, createProbot } from "../utils";
+import { course, repo } from "../course";
 import { bots } from "../config";
-
-export const app = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    // const issueComment = context.issue({
-    //   body: "Thanks for opening this issue!",
-    // });
-    // await context.octokit.issues.createComment(issueComment);
-  });
-
-  app.on(["installation", "installation_repositories"], async (context) => {
-    // context.log("Root installed");
-  });
-};
 
 export const instance = createProbot(bots.root);
 export const config = bots.root;
+
+export const app = (app: Probot) => {
+  const triggerActions = Course.getTriggers(course);
+  for (const action of triggerActions) {
+    const { event, handler } = action.passed;
+    app.on(event as any, async (context) => {
+      if (context.payload.repository.name !== repo) return;
+
+      const passed = (handler as any)(context.payload);
+      if (!passed) return;
+
+      const metadata = new Metadata(context.octokit, getMetaIssue(context));
+      const added = await metadata.add("triggers", action.id);
+
+      if (added) {
+        emitter.emit("clientUpdate", {
+          type: "trigger:passed",
+          username: context.payload.sender.login,
+          repo,
+          actionId: action.id,
+        });
+      }
+    });
+  }
+};
+
+function getMetaIssue(context: any) {
+  return {
+    owner: context.payload.repository.owner.login,
+    repo,
+    issue_number: 1,
+  };
+}
