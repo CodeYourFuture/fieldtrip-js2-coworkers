b1757a39f70c026b6c9fc23434742e0e2a87b524
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2022-01-28 15:21:55 +0000

    feat: pass data from hooks to milestones/actions

--- stages/week1.ts
+++ stages/week1.ts
@@ -15,10 +15,12 @@ export const week1: CourseStage = {
     {
       id: "meet-malachi",
       label: "Meet Malachi",
-      url: (state) => `${state.enrollment.repoUrl}/issues/1`,
+      url: (state) =>
+        `${state.enrollment.repoUrl}/issues/${state.hooks["malachi-intro"].number}`,
       passed: on(
         "issues.closed",
-        (event) => event.issue.title === "Introducing your product owner"
+        (event, state) =>
+          event.issue.number === state.hooks["malachi-intro"].number
       ),
     },
     {
@@ -30,10 +32,11 @@ export const week1: CourseStage = {
     {
       id: "meet-uma",
       label: "Meet Uma",
-      url: (state) => `${state.enrollment.repoUrl}/issues/2`,
+      url: (state) =>
+        `${state.enrollment.repoUrl}/issues/${state.hooks["uma-intro"].number}`,
       passed: on(
         "issues.closed",
-        (event) => event.issue.title === "Introducing your technical lead"
+        (event, state) => event.issue.number === state.hooks["uma-intro"].number
       ),
     },
     {
@@ -45,11 +48,11 @@ export const week1: CourseStage = {
   ],
   milestones: [
     {
-      id: "merge-setup-pr",
+      id: "merge-initial-pr",
       label: "Merge setup PR",
       passed: on(
         "pull_request.closed",
-        (event) => event.pull_request.title === "Set up repo"
+        (event, state) => event.pull_request.id === state.hooks["initial-pr"].id
       ),
     },
     {
@@ -58,17 +61,123 @@ export const week1: CourseStage = {
       passed: on(
         "issues.assigned",
         (event) =>
-          event.issue.title === "Store member data for use in digital tools"
+          // event.issue.title === "Store member data for use in digital tools"
+          event.issue.title === "Set up repo"
       ),
     },
     {
-      id: "move-first-issue",
+      id: "move-initial-card",
       label: "Move first issue to In Progress",
-      passed: false,
-      // on(
-      // "project_card.moved",
-      // (event, state) => event.project_card.column_id === state.getColumnId("in-progress")
-      // ),
+      passed: on(
+        "project_card.moved",
+        (event, state) =>
+          event.project_card.id === state.hooks["initial-card"].id &&
+          event.project_card.column_id ===
+            state.hooks["board-setup"].columns[1].id
+      ),
+    },
+  ],
+  hooks: [
+    {
+      id: "malachi-intro",
+      hook: on.malachi(
+        ["installation_repositories.added", "installation.created"],
+        () => true,
+        (malachi) => {
+          return malachi.createIssue({
+            title: "Introducing your product owner",
+            body: "week1/malachi/intro.md",
+          });
+        }
+      ),
+    },
+    {
+      id: "board-setup",
+      hook: on.malachi(
+        ["installation_repositories.added", "installation.created"],
+        () => true,
+        async (malachi) => {
+          const project = await malachi.createProject({
+            name: "Co-worker tools",
+            body: "A collection of tools for co-workers",
+          });
+
+          const columnNames = ["Todo", "In Progress", "Blocked", "Done"];
+
+          const columns = await Promise.all(
+            columnNames.map((columnName) =>
+              malachi.createProjectColumn({
+                projectId: project.id,
+                name: columnName,
+              })
+            )
+          );
+
+          return {
+            id: project.id,
+            columns: columns.map((column) => ({
+              id: column.id,
+              name: column.name,
+            })),
+          };
+        }
+      ),
+    },
+
+    {
+      id: "initial-card",
+      hook: on.malachi(
+        ["installation_repositories.added", "installation.created"],
+        () => true,
+        async (malachi, state) => {
+          const task = await malachi.createIssue({
+            title: "Set up repo",
+            body: "./week1/tasks/set-up-repo.md",
+          });
+
+          return malachi.createProjectCard({
+            columnId: state.hooks["board-setup"].columns[0].id,
+            issueNumber: task.id,
+          });
+        }
+      ),
+    },
+    {
+      id: "uma-intro",
+      hook: on.uma(
+        ["installation_repositories.added", "installation.created"],
+        () => true,
+        (uma) => {
+          return uma.createIssue({
+            title: "Introducing your technical lead",
+            body: "week1/uma/intro.md",
+          });
+        }
+      ),
+    },
+    {
+      id: "initial-pr",
+      hook: on.uma(
+        ["installation_repositories.added", "installation.created"],
+        () => true,
+        async (uma) => {
+          await uma.createBranch("setup-repo");
+
+          await uma.updateFile({
+            path: "README.md",
+            content: "#JS",
+            branch: "setup-repo",
+          });
+
+          return uma.createPullRequest({
+            from: "setup-repo",
+            to: "main",
+            title: "Set up repo",
+            body: "Set up repo",
+            reviewers: [uma.user.login],
+          });
+        }
+      ),
     },
   ],
 };
